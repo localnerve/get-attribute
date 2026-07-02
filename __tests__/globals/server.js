1417fe76d1691bc12c5d1b47b033ecd344a30b36
localnerve <alex@localnerve.com>
2026-07-02 09:09:08 +0000

    @5.3.0 - fix lint

--- __tests__/globals/server.js
+++ __tests__/globals/server.js
@@ -27,7 +27,7 @@ export const start = (rootDir, port, cb) => {
   });
   server.post('/shutdown', (req, res) => { // for e2e cli testing
     res.sendStatus(200);
-    process.exit(0);
+    process.exit(0); // eslint-disable-line n/no-process-exit
   });
   const httpServer = server.listen(parseInt(port, 10), err => {
     if (cb) {
